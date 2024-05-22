a7877fff72577a46ee3f1d84919a43b19acecaa0
Dawid Malinowski <dawidmalina@gmail.com>
2024-05-22 11:28:54 +0200

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,20 +1,23 @@
 const inputs = process.argv
 
-const approvers = inputs[0]
-const actor = inputs[1]
+const approvers = inputs[2]
+const actor = inputs[3]
 
 console.log(approvers)
 console.log(actor)
 
+const aaaa = JSON.stringify(approvers)
+console.log(aaaa)
+
 if (Array.isArray(approvers)) {
   // check if actor is in the approvers list
-  if (approvers.contains(actor) {
-    console.log("actor is valid approvers")  
+  if (approvers.contains(actor)) {
+    console.log("actor is valid approvers")
   } else {
-    console.log("actor is not valid approvers")  
-    proces.exit(1)
+    console.log("actor is not valid approvers")
+    process.exit(1)
   }
 } else {
-  console.log("approvers is not a list")
-  proces.exit(1)
+  console.log(`approvers ${approvers} is not valid array`)
+  process.exit(1)
 }
